13ae2165db4586cb047c3a80f0628e0c9cda2fad
LingRen <976754395@qq.com>
2018-03-16 09:27:48 +0800

    更新init 配置

--- meta.js
+++ meta.js
@@ -1,25 +1,67 @@
 module.exports = {
-	helpers: {
-		escape: function (value) {
-			return value.replace(/'/g, '&apos;');
-		}
-	},
 	prompts: {
 		name: {
-			'type': 'string',
-			'required': true,
-			'message': 'Project name'
+			when: 'isNotTest',
+			type: 'string',
+			required: true,
+			message: 'Project name',
 		},
 		description: {
-			'type': 'string',
-			'required': false,
-			'message': 'Project description',
-			'default': 'A Vue.js project'
+			when: 'isNotTest',
+			type: 'string',
+			required: false,
+			message: 'Project description',
+			default: 'A Vue.js project',
 		},
 		author: {
-			'type': 'string',
-			'message': 'Author'
+			when: 'isNotTest',
+			type: 'string',
+			message: 'Author',
 		},
+		autoInstall: {
+			when: 'isNotTest',
+			type: 'list',
+			message: 'Should we run `npm install` for you after the project has been created? (recommended)',
+			choices: [{
+					name: 'Yes, use NPM',
+					value: 'npm',
+					short: 'npm',
+				},
+				{
+					name: 'Yes, use Yarn',
+					value: 'yarn',
+					short: 'yarn',
+				},
+				{
+					name: 'No, I will handle that myself',
+					value: false,
+					short: 'no',
+				},
+			]
+		}
 	},
-	completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
+	complete: function (data, {
+		chalk
+	}) {
+		const green = chalk.green
+
+		sortDependencies(data, green)
+
+		const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
+
+		if (data.autoInstall) {
+			installDependencies(cwd, data.autoInstall, green)
+				.then(() => {
+					return runLintFix(cwd, data, green)
+				})
+				.then(() => {
+					printMessage(data, green)
+				})
+				.catch(e => {
+					console.log(chalk.red('Error:'), e)
+				})
+		} else {
+			printMessage(data, chalk)
+		}
+	},
 };
