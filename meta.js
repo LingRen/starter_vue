53c9ab54d5e35241c3e01785c4f7e81054128ee4
LingRen <976754395@qq.com>
2018-03-16 09:30:23 +0800

    更新

--- meta.js
+++ meta.js
@@ -1,22 +1,19 @@
 module.exports = {
 	prompts: {
 		name: {
-			when: 'isNotTest',
-			type: 'string',
-			required: true,
-			message: 'Project name',
+			'type': 'string',
+			'required': true,
+			'message': 'Project name'
 		},
 		description: {
-			when: 'isNotTest',
-			type: 'string',
-			required: false,
-			message: 'Project description',
-			default: 'A Vue.js project',
+			'type': 'string',
+			'required': false,
+			'message': 'Project description',
+			'default': 'A Vue.js project'
 		},
 		author: {
-			when: 'isNotTest',
-			type: 'string',
-			message: 'Author',
+			'type': 'string',
+			'message': 'Author'
 		},
 		autoInstall: {
 			when: 'isNotTest',
